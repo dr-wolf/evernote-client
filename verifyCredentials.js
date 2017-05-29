a34824a23556d214e827078b96fc3a2f00a265a6
Roman <roman.grunah@gmail.com>
2017-05-29 15:54:53 +0300

    evernote

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,36 +1,12 @@
 "use strict";
-const request = require('request-promise');
-
 module.exports = verify;
 
-/**
- * Executes the verification logic by sending a simple to the Petstore API using the provided apiKey.
- * If the request succeeds, we can assume that the apiKey is valid. Otherwise it is not valid.
- *
- * @param credentials object to retrieve apiKey from
- *
- * @returns Promise sending HTTP request and resolving its response
- */
 function verify(credentials) {
-
-    // access the value of the apiKey field defined in credentials section of component.json
     const apiKey = credentials.apiKey;
+    const client = new Evernote.Client({token: apiKey, sandbox: true});
+    const noteStore = client.getNoteStore();
 
-    if (!apiKey) {
-        throw new Error('API key is missing');
-    }
-
-    // sending a request to the most simple endpoint of the target API
-    const requestOptions = {
-        uri: 'https://sandbox.evernote.com/shard/s1/notestore',
-        headers: {
-            'oauth_token': apiKey
-        },
-        json: true
-    };
-
-    // if the request succeeds, we can assume the api key is valid
-    return rq(requestOptions).then(function (response) {
+    noteStore.listNotebooks().then(function(notebooks) {
         return cb(null, {verified: true});
     }).catch(function (err) {
         return cb(err);
